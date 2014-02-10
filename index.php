c8bfd6dc8fbaf9de05d4dd9088a34469d3765dd5
Czarnodziej <pagodemc@gmail.com>
2014-02-10 12:43:48 +0100

    homepage view fix

--- index.php
+++ index.php
@@ -69,13 +69,12 @@
           niezbędna jest aktualna przeglądarka internetowa.
           <br>W planach są:
         </p>
-        <ul class="icons-ul">
-          <li><i class="icon-li icon-screenshot icon-large"></i>Konstrukcja blogowa z wykorzystaniem frameworka Cake PHP</li>
-          <li><i class="icon-li icon-screenshot icon-large"></i>Wielce użyteczne formularze z wykorzystaniem AJAX</li>
-          <li><i class="icon-li icon-screenshot icon-large"></i>Wykorzystanie Javascript do stworzenia prostej gry przeglądarkowej</li>
-          <li><i class="icon-li icon-screenshot icon-large"></i>Kilka artykułów nt. technologii internetowo-komputerowych z naciskiem na dostępność dla zwykłych użytkowników</li>
-        </ul>
-        <p>Wszystkie powyższe założenia będą wdrażane z zachowaniem wysokiej przejrzystości strony i dostępności zawartych tutaj informacji. Idąc rozwojowym torem,
+       
+<ul class="icons-ul">
+  <li><i class="icon-li icon-screenshot icon-large"></i>
+	<a href="http://insanet.pl">Rozwój założeń projektu</a></li>
+</ul>
+<p>Wszystkie powyższe założenia będą wdrażane z zachowaniem wysokiej przejrzystości strony i dostępności zawartych tutaj informacji. Idąc rozwojowym torem,
           powyższych celów będzie więcej, bo apetyt na ów rozwój rośnie wraz z ilością poznawanych technologii (<i class="icon-food icon-large"></i> + <i class="icon-cogs icon-large"></i> =
           <i class="icon-smile icon-large"></i>). A tych nie brakuje.</p>
  	Wersje strony:
